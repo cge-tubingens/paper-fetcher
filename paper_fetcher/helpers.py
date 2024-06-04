89f96f06e82670f750fe3411bd698fb18a488479
Luis Giraldo <luisggon@protonmail.com>
2024-06-04 14:40:59 +0200

    functions to fetch and get pub data

---
+++ paper_fetcher/helpers.py
@@ -0,0 +1,69 @@
+
+
+import requests
+from xml.etree import ElementTree
+import csv
+
+from bs4.element import Tag
+from bs4 import BeautifulSoup
+
+# Function to get all PubMed IDs related to a search term, excluding preprints
+def get_all_pubmed_ids(search_term, batch_size=100):
+    pubmed_ids = []
+    retstart = 0
+    while True:
+        # Exclude preprints by adding "-medrxiv[All Fields] -biorxiv[All Fields]" to the search term
+        esearch_url = (
+            f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
+            f"db=pubmed&term={search_term} NOT medrxiv[All Fields] NOT biorxiv[All Fields] NOT ArXiv[All Fields]" 
+            f"&retmax={batch_size}&retstart={retstart}&retmode=xml"
+        )
+        esearch_response = requests.get(esearch_url)
+        esearch_response.raise_for_status()
+
+        esearch_tree = ElementTree.fromstring(esearch_response.content)
+        id_list = [id_elem.text for id_elem in esearch_tree.findall(".//Id")]
+        pubmed_ids.extend(id_list)
+        
+        if len(id_list) < batch_size:
+            break
+        
+        retstart += batch_size
+    
+    return pubmed_ids
+
+
+def get_pub_details(web_address:str)->dict:
+
+    # Create a handle, page, to handle the contents of the website
+    data = requests.get(web_address).text
+
+    # Creating BeautifulSoup object
+    soup = BeautifulSoup(data, 'html.parser')
+
+    result_dict = {}
+
+    result_dict['title'] = soup.find_all('meta', {"name":"citation_title"})[0]['content']
+    result_dict['journal'] = soup.find_all('meta', {"name":"citation_journal_title"})[0]['content']
+    result_dict['year'] = soup.find('time', {"class":"citation-year"}).text
+    result_dict['authors'] = []
+ 
+    auth_list = soup.find_all('div', {"class":"authors-list"})[0]
+    
+    info = []
+    for th in auth_list.children:
+        if isinstance(th, Tag):
+            info.append(th)
+    
+    for elem in info:
+
+        auth_name = elem.find('a', {"class":"full-name"}).text
+        if elem.find('a', {"class":"affiliation-link"}) is not None:
+            auth_aff = elem.find('a', {"class":"affiliation-link"})['title']
+        else:
+            auth_aff = None
+        result_dict["authors"].append(
+            (auth_name, auth_aff)
+        )
+
+    return result_dict
