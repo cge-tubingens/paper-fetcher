3e2e603c1f21a89db547361f1bb2e8accc9e58d9
Luis Giraldo <luisggon@protonmail.com>
2024-06-11 16:15:52 +0200

    function to recover col names

--- paper_fetcher/utils.py
+++ paper_fetcher/utils.py
@@ -127,3 +127,29 @@ def arg_parser()->dict:
     args = parser.parse_args()
 
     return args
+
+def recover_columns_names(columns:list)->list:
+
+    """
+    Recover the original column names from a list of transformed column names.
+
+    Parameters:
+    -----------
+    columns : list
+        List of transformed column names.
+
+    Returns:
+    --------
+    old_columns : list
+        List of original column names.
+    """
+
+    old_columns = []
+
+    for col in columns:
+        splitted = col.split('__')
+        if len(splitted)==2:
+            old_columns.append(splitted[1])
+        else:
+            old_columns.append(splitted[1] + '__' + splitted[2])
+    return old_columns
